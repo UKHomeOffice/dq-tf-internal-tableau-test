4abc11b7434bda077158b42927b6f2274b01cdfa
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-01-12 10:39:14 +0000

    Added rds back in

--- rds.tf
+++ rds.tf
@@ -1,15 +1,15 @@
-#resource "aws_db_subnet_group" "rds" {
-#  name = "internal_tableau_rds_group"
-#
-#  subnet_ids = [
-#    "${aws_subnet.subnet.id}",
-#    "${aws_subnet.internal_tableau_az2.id}",
-#  ]
-#
-#  tags {
-#    Name = "rds-subnet-group-${local.naming_suffix}"
-#  }
-#}
+resource "aws_db_subnet_group" "rds" {
+  name = "internal_tableau_rds_group"
+
+  subnet_ids = [
+    "${aws_subnet.subnet.id}",
+    "${aws_subnet.internal_tableau_az2.id}",
+  ]
+
+  tags {
+    Name = "rds-subnet-group-${local.naming_suffix}"
+  }
+}
 
 resource "aws_subnet" "internal_tableau_az2" {
   vpc_id                  = "${var.appsvpc_id}"
@@ -69,30 +69,30 @@ resource "aws_security_group" "internal_tableau_db" {
   }
 }
 
-#resource "aws_db_instance" "postgres" {
-#  identifier              = "int-tableau-postgres-${local.naming_suffix}"
-#  allocated_storage       = 10
-#  storage_type            = "gp2"
-#  engine                  = "postgres"
-#  engine_version          = "10.4"
-#  instance_class          = "db.t2.small"
-#  username                = "${random_string.username.result}"
-#  password                = "${random_string.password.result}"
-#  backup_window           = "00:00-01:00"
-#  maintenance_window      = "mon:01:30-mon:02:30"
-#  backup_retention_period = 14
-#  storage_encrypted       = true
-#  multi_az                = true
-#  skip_final_snapshot     = true
+resource "aws_db_instance" "postgres" {
+  identifier              = "int-tableau-postgres-${local.naming_suffix}"
+  allocated_storage       = 10
+  storage_type            = "gp2"
+  engine                  = "postgres"
+  engine_version          = "10.4"
+  instance_class          = "db.t2.small"
+  username                = "${random_string.username.result}"
+  password                = "${random_string.password.result}"
+  backup_window           = "00:00-01:00"
+  maintenance_window      = "mon:01:30-mon:02:30"
+  backup_retention_period = 14
+  storage_encrypted       = true
+  multi_az                = true
+  skip_final_snapshot     = true
 
-#  db_subnet_group_name   = "${aws_db_subnet_group.rds.id}"
-#  vpc_security_group_ids = ["${aws_security_group.internal_tableau_db.id}"]
+  db_subnet_group_name   = "${aws_db_subnet_group.rds.id}"
+  vpc_security_group_ids = ["${aws_security_group.internal_tableau_db.id}"]
 
-#  lifecycle {
-#    prevent_destroy = true
-#  }
+  lifecycle {
+    prevent_destroy = true
+  }
 
-#  tags {
-#    Name = "internal_tableau-postgres-${local.naming_suffix}"
-#  }
-#}
+  tags {
+    Name = "internal_tableau-postgres-${local.naming_suffix}"
+  }
+}
