eebfae13e1b8143fc9950d56d75664eaa3bd8d92
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-02-01 10:48:28 +0000

    Recreating rds as t3

--- rds.tf
+++ rds.tf
@@ -90,7 +90,7 @@ resource "aws_db_instance" "postgres" {
   storage_type            = "gp2"
   engine                  = "postgres"
   engine_version          = "10.4"
-  instance_class          = "db.t2.small"
+  instance_class          = "db.t3.small"
   username                = "${random_string.username.result}"
   password                = "${random_string.password.result}"
   name                    = "${var.database_name}"
