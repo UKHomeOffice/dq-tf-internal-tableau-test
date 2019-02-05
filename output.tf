82e0c6248efe9241b9a7c6e647e2d1c6b2fc40ae
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-02-05 18:40:23 +0000

    Add hostname output

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "rds_internal_tableau_endpoint" {
   value = "${aws_db_instance.postgres.endpoint}"
 }
+
+output "rds_internal_tableau_hostname" {
+  value = "${aws_db_instance.postgres.hostname}"
+}
