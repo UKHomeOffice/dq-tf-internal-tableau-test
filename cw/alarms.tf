3248536a7f6a75ca3a18fd49ad8384c379282e54
JWanderema <wanderema@hotmail.co.uk>
2021-06-15 13:25:36 +0100

    add cw

---
+++ cw/alarms.tf
@@ -0,0 +1,105 @@
+locals {
+  path_module = var.path_module != "unset" ? var.path_module : path.module
+
+  thresholds = {
+    CPUUtilizationThreshold   = min(max(var.cpu_utilization_threshold, 0), 100)
+    AvailableMemoryThreshold  = max(var.available_memory_threshold, 0)
+    UsedStorageSpaceThreshold = max(var.used_storage_space_threshold, 0)
+
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "cpu_utilization_too_high" {
+  count               = var.environment == "prod" ? "2" : "1"
+  alarm_name          = "${var.pipeline_name}-CPU-Utilization-too-high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "600"
+  statistic           = "Average"
+  threshold           = local.thresholds["CPUUtilizationThreshold"]
+  alarm_description   = "Average EC2 Instance CPU utilization over last 10 minutes too high"
+  alarm_actions       = [aws_sns_topic.ec2.arn]
+  ok_actions          = [aws_sns_topic.ec2.arn]
+
+  dimensions = {
+    InstanceId = aws_instance.int_tableau_linux[count.index].id
+  }
+
+  depends_on = [
+    aws_instance.int_tableau_linux[count.index].id
+  ]
+
+}
+
+# resource "aws_cloudwatch_metric_alarm" "available_memory_too_low" {
+#   count               = var.environment == "prod" ? "2" : "1"
+#   alarm_name          = "${var.pipeline_name}-available-memory-too-low"
+#   comparison_operator = "LessThanThreshold"
+#   evaluation_periods  = "1"
+#   metric_name         = "mem_available_percent"
+#   namespace           = "CWAgent"
+#   period              = "600"
+#   statistic           = "Average"
+#   threshold           = local.thresholds["AvailableMemoryThreshold"]
+#   alarm_description   = "Average available memory over last 10 minutes"
+#   alarm_actions       = [aws_sns_topic.ec2.arn]
+#   ok_actions          = [aws_sns_topic.ec2.arn]
+#
+#   dimensions = {
+#     InstanceId = aws_instance.int_tableau_linux[count.index].id
+#   }
+#
+#   depends_on = [
+#     aws_instance.int_tableau_linux[count.index].id
+#   ]
+# }
+#
+# resource "aws_cloudwatch_metric_alarm" "Used_storage_space" {
+#   count               = var.environment == "prod" ? "2" : "1"
+#   alarm_name          = "${var.pipeline_name}-used-storage-space"
+#   comparison_operator = "GreaterThanThreshold"
+#   evaluation_periods  = "1"
+#   metric_name         = "disk_used_percent"
+#   namespace           = "CWAgent"
+#   period              = "600"
+#   statistic           = "Average"
+#   threshold           = local.thresholds["UsedStorageSpaceThreshold"]
+#   alarm_description   = "Average database free storage space over last 10 minutes too low"
+#   alarm_actions       = [aws_sns_topic.ec2.arn]
+#   ok_actions          = [aws_sns_topic.ec2.arn]
+#
+#   dimensions = {
+#     InstanceId = aws_instance.int_tableau_linux[count.index].id,
+#     path       = "/",
+#     fstype     = "xfs",
+#   }
+#
+#   depends_on = [
+#     aws_instance.int_tableau_linux[count.index].id
+#   ]
+# }
+#
+# resource "aws_cloudwatch_metric_alarm" "health" {
+#   count = var.environment == "prod" ? "2" : "1"
+#   alarm_name          = "web-health-alarm"
+#   comparison_operator = "GreaterThanOrEqualToThreshold"
+#   evaluation_periods  = "1"
+#   metric_name         = "StatusCheckFailed"
+#   namespace           = "AWS/EC2"
+#   period              = "120"
+#   statistic           = "Average"
+#   threshold           = "1"
+#   alarm_description   = "This metric monitors ec2 health status"
+#   alarm_actions       = [aws_sns_topic.ec2.arn]
+#   ok_actions          = [aws_sns_topic.ec2.arn]
+#
+#   dimensions = {
+#     InstanceId = aws_instance.int_tableau_linux[count.index].id
+#   }
+#
+#   depends_on = [
+#     aws_instance.int_tableau_linux[count.index].id
+#   ]
+# }
