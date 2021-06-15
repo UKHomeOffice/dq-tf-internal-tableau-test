76626dd2f13b0df9a7cb2ef7081d561953ad2998
JWanderema <wanderema@hotmail.co.uk>
2021-06-15 13:39:38 +0100

    add ec2

--- cw/variable.tf
+++ cw/variable.tf
@@ -1,31 +1,31 @@
-variable "pipeline_name" {
-  description = "CloudWatchAlarm"
-  default     = "internal_tableau"
-}
-
-variable "swap_alarm" {
-  description = "Switch to turn off Swap monitoring (required for MSSQL). Accepted values are 'false' to turn off and 'true' to excplicitly turn on"
-  default     = "true"
-}
-
-variable "path_module" {
-  default = "unset"
-}
-
-variable "cpu_utilization_threshold" {
-  description = "The maximum percentage of CPU utilization."
-  type        = string
-  default     = 80
-}
-
-variable "available_memory_threshold" {
-  description = "The percentage of available memory."
-  type        = string
-  default     = 20
-}
-
-variable "used_storage_space_threshold" {
-  description = "The minimum amount of available storage space in Byte."
-  type        = string
-  default     = 80
-}
+# variable "pipeline_name" {
+#   description = "CloudWatchAlarm"
+#   default     = "internal_tableau"
+# }
+#
+# variable "swap_alarm" {
+#   description = "Switch to turn off Swap monitoring (required for MSSQL). Accepted values are 'false' to turn off and 'true' to excplicitly turn on"
+#   default     = "true"
+# }
+#
+# variable "path_module" {
+#   default = "unset"
+# }
+#
+# variable "cpu_utilization_threshold" {
+#   description = "The maximum percentage of CPU utilization."
+#   type        = string
+#   default     = 80
+# }
+#
+# variable "available_memory_threshold" {
+#   description = "The percentage of available memory."
+#   type        = string
+#   default     = 20
+# }
+#
+# variable "used_storage_space_threshold" {
+#   description = "The minimum amount of available storage space in Byte."
+#   type        = string
+#   default     = 80
+# }
--- cw/versions.tf
+++ cw/versions.tf
@@ -1,16 +1,16 @@
-terraform {
-  required_version = ">= 0.12"
-  required_providers {
-    aws    = "~> 2.70"
-    random = "~> 2.3"
-  }
-
-}
-
-module "aws_global" {
-  providers = {
-    aws.global = aws
-  }
-  source = "../mymodule/cloudwatch-ec2"
-
-}
+# terraform {
+#   required_version = ">= 0.12"
+#   required_providers {
+#     aws    = "~> 2.70"
+#     random = "~> 2.3"
+#   }
+#
+# }
+#
+# module "aws_global" {
+#   providers = {
+#     aws.global = aws
+#   }
+#   source = "../mymodule/cloudwatch-ec2"
+#
+# }
