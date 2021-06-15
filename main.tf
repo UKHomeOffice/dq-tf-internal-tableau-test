4856c542bd2020009731173ee5409f930451667f
JWanderema <wanderema@hotmail.co.uk>
2021-06-15 14:48:41 +0100

    mod

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ module "cloudwatch_alarms_ec2" {
   source          = "github.com/UKHomeOffice/dq-tf-cloudwatch-ec2"
   environment     = "test"
   naming_suffix   = local.naming_suffix
-  ec2_instance_id = aws_instance.int_tableau_linux.id
+  ec2_instance_id = aws_instance.int_tableau_linux[count.index].id
   pipeline_name   = "internal_tableau"
 }
 
