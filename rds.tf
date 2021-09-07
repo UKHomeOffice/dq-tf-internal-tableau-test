ab71fa4ac085ebd86f85e1cd79c8cd367427907d
JWanderema <wanderema@hotmail.co.uk>
2021-09-07 16:40:01 +0100

    upgrade db

--- rds.tf
+++ rds.tf
@@ -103,7 +103,7 @@ resource "aws_db_instance" "postgres" {
   allocated_storage               = "350"
   storage_type                    = "gp2"
   engine                          = "postgres"
-  engine_version                  = "10.15"
+  engine_version                  = "10.17"
   instance_class                  = "db.m5.2xlarge"
   enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
   username                        = random_string.username.result
