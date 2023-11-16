07b62da92fb044e0ce27712029474449afabf9af
Srikanth Arunachalam <116078089+s-arunachalam@users.noreply.github.com>
2023-11-16 13:07:34 +0000

    feat: yel-8578 reverted to remove data block with specific cidrs

--- data.tf
+++ data.tf
@@ -33,38 +33,3 @@ data "aws_ami" "int_tableau_linux_upgrade" {
 data "aws_kms_key" "rds_kms_key" {
   key_id = "alias/aws/rds"
 }
-
-data "aws_subnet" "sub_dq_internal_dashboard_subnet_cidr" {
-  filter {
-    name   = "tag:Name"
-    values = ["subnet-internal-tableau-apps-*"]    
-  }
-}
-
-data "aws_subnet" "sub_dq_internal_dashboard_subnet_cidr_az2" {
-  filter {
-    name   = "tag:Name"
-    values = ["az2-subnet-internal-tableau-apps-*"]    
-  }
-}
-
-data "aws_subnet" "sub_dq_external_dashboard_subnet_cidr" {
-  filter {
-    name   = "tag:Name"
-    values = ["subnet-external-tableau-apps-*"]    
-  }
-}
-
-data "aws_subnet" "sub_dq_lambda_apps_cidr" {
-  filter {
-    name   = "tag:Name"
-    values = ["subnet-lambda-apps-*"]    
-  }
-}
-
-data "aws_subnet" "sub_dq_lambda_apps_cidr_az2" {
-  filter {
-    name   = "tag:Name"
-    values = ["az2-subnet-lambda-apps-*"]    
-  }
-}
