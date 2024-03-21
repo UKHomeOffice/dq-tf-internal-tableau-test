7e35fd962b20ec9ffef0fb6bc58ff9f2bf6ff8f6
blake-mck-HO <DEV\xd-mckenzb@a-2dwah8l6xsuds.dev.dacc.phz>
2024-03-21 11:40:50 +0000

    changed source dir and output path to remove local path module due to drone error

--- variables.tf
+++ variables.tf
@@ -242,9 +242,6 @@ variable "rds_enhanced_monitoring_role" {
 #   path_module = var.path_module != "unset" ? var.path_module : path.module
 # }
 #
-variable "path_module" {
-  default = "unset"
-}
 
 ############################
 #Cloudwatch Alarm variables#
